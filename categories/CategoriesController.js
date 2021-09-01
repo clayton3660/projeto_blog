d396d5882c7c9fa79b1241a69a8c0b3210555730
Clayton Silva <clayton3660@gmail.com>
2021-09-01 12:14:12 -0300

    Formulario_de_categoria

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,12 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/categories', (req, res) => {
-  res.send('Rota de categorias');
+router.get('/admin/categories/new', (req, res) => {
+  res.render('admin/categories/new');
 });
-
-router.get('/admin/catergories/new', (req, res) => {
-  res.send('rota para criar uma nova catergoria');
-});
-
 module.exports = router;
