a3186c6eb339547ecb420700a74b24aa53f41fb5
Clayton Silva <clayton3660@gmail.com>
2021-09-04 00:32:26 -0300

    Cadastro_de_categorias_e_slugify

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const Category = require('./Category'); // -> importar para ter comandos para inserir no banco de dados
+const slugify = require('slugify');
 
 router.get('/admin/categories/new', (req, res) => {
   res.render('admin/categories/new');
 });
+
+router.post('/categories/save', (req, res) => {
+  var title = req.body.title;
+  if (title != undefined) {
+    Category.create({
+      // -> comandos para inserir no banco de dados
+      title: title,
+      slug: slugify(title), // utilizado para colocar tudo em minusculo e remover os espaços
+    }).then(() => {
+      res.redirect('/');
+    });
+  } else {
+    res.redirect('/admin/categories/new');
+  }
+});
+
 module.exports = router;
