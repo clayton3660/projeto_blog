5ff8ff16f859ebb43c80e731d67a601c66b32300
Clayton Silva <clayton3660@gmail.com>
2021-09-25 10:23:50 -0300

    Impedindo_emails_duplicados

--- users/UsersController.js
+++ users/UsersController.js
@@ -1,31 +1,44 @@
 const express = require('express');
 const router = express.Router();
 const User = require('./User');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
 
 router.get('/admin/users', (req, res) => {
-  res.send('Listagem de usuários');
+  // res.send('Listagem de usuários');
+  User.findAll().then((users) => {
+    res.render('admin/users/index', { users: users });
+  });
 });
 
 router.get('/admin/users/create', (req, res) => {
   res.render('admin/users/create');
 });
 
-router.post("/users/create", (req, res) => {
+router.post('/users/create', (req, res) => {
   var email = req.body.email;
   var password = req.body.password;
 
-  var salt = bcrypt.genSaltSync(10);
-  var hash = bcrypt.hashSync(password, salt);
+  User.findOne({ where: { email: email } }).then((user) => {
+    if (user == undefined) {
+      var salt = bcrypt.genSaltSync(10);
+      var hash = bcrypt.hashSync(password, salt);
 
-  User.create({
-    email: email,
-    password: hash
-  }).then(() => {
-    res.redirect('/')
-  }).catch((err) => {
-    res.redirect('/')
-  })
-})
+      User.create({
+        email: email,
+        password: hash,
+      })
+        .then(() => {
+          //res.redirect('/admin/users');
+          res.redirect('/');
+        })
+        .catch((err) => {
+          res.redirect('/');
+          //res.redirect('/admin/users');
+        });
+    } else {
+      res.redirect('/admin/users/create');
+    }
+  });
+});
 
 module.exports = router;
